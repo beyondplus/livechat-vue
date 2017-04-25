c29f2a8e1c7f37d60a90977614231da52126a937
San Pwint Thu <bplusmyanmar9@hotmail.com>
2017-04-25 13:51:45 +0630

    session function added

--- php/fetch.php
+++ php/fetch.php
@@ -1,5 +1,23 @@
 <?php 
+// remove all session variables
+session_start();
 
-echo json_encode(array("user" => "Mg Mg" ,"text" => "I'm here" ));
+if(isset($_POST['user'] )) {
+
+	$user = $_POST['user'];
+	$text = $_POST['text'];
+	$data = array('user' => $user, 'text' => $text);
+	//This is using session . You can change database insert query and select query
+	$_SESSION['data'][] = $data;
+
+	echo json_encode(array($_SESSION));	
+}
+
+echo json_encode($_SESSION);	
+
+
+// session_unset(); 
+// // destroy the session 
+// session_destroy(); 
 
 ?>
--- php/post.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-echo json_encode(array("user" => "Mg Mg" ,"text" => "I'm here" ));
-
-?>
